159d3f5f3b685510cf7abd705a7af752cfc7ccf6
hilalb-sahin <114245624+hilalb-sahin@users.noreply.github.com>
2022-09-24 22:10:30 +0100

    Update interacting.js

--- interacting.js
+++ interacting.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-
 
 let mysteryButton = document.getElementById('mysteryButton');
 let background = document.getElementsByClassName('color')[0];
